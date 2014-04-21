156323a3a8dc4178af2e56af016995e55832e717
Jan Vlnas <git@jan.vlnas.cz>
2014-04-22 01:35:42 +0200

    Rebind instance and call onBeforeSubmit via Entwine

--- javascript/sirtrevor.js
+++ javascript/sirtrevor.js
@@ -1,10 +1,36 @@
+
 (function($) {
     'use strict';
-    $.entwine('sirtrevorfield', function($){
-      $('.js-sirtrevorfield').entwine({
+
+    $.entwine('sirtrevoreditor', function($){
+      // new SirTrevor.Editor({ el: $('.js-sirtrevorfield') });
+      $('textarea.sirtrevoreditor').entwine({
+        Editor: null,
+        onadd: function() {
+          console.log('onadd');
+        },
+        getEditor: function() {
+        },
         onmatch: function() {
-          this.instance = new SirTrevor.Editor({ el: this });
-        }
+          // FIXME: this prohibits multiple instances on same page
+          var editor = SirTrevor.getInstance();
+          console.log(editor);
+          if(editor) {
+            editor.reinitialize({el: this});
+          }
+          else {
+            new SirTrevor.Editor({ el: this });
+          }
+        },
+
+        'from .cms-edit-form': {
+          onbeforesubmitform: function(e) {
+            // this.getEditor().save();
+            SirTrevor.onBeforeSubmit();
+            console.log('onbeforesubmitform');
+            this._super();
+          }
+        },
       });
     });
 
